7e80bea96e6ca5f9a84af8d750311fc386c12937
kissge <mail@yo.eki.do>
2016-11-23 23:09:30 +0900

    Add Linux support (Use md5sum or md5 depending platform)

--- src/histfile.zsh
+++ src/histfile.zsh
@@ -3,10 +3,15 @@
 # Histfile Helper Functions                                          #
 #--------------------------------------------------------------------#
 
+# Universal (Linux/BSD) md5 utility
+_zsh_cwd_history_md5() {
+	which md5 > /dev/null 2>&1 && md5 -q || (md5sum | cut -c-32)
+}
+
 # Echo the name of the history file to use for current working dir
 # Uses the md5 hash of the absolute path
 _zsh_cwd_history_histfile_for_cwd() {
-	echo "$ZSH_CWD_HISTORY_DIR/.hist-$(echo "${PWD:A}" | md5 -q)"
+	echo "$ZSH_CWD_HISTORY_DIR/.hist-$(echo "${PWD:A}" | _zsh_cwd_history_md5)"
 }
 
 # Switch to a new history file, writing to the old one
