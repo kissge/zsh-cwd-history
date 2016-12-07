f7a2c82c574c72b4a3c32be6d5e2677050f3c791
kissge <mail@yo.eki.do>
2016-12-07 14:09:03 +0900

    Build

--- zsh-cwd-history.zsh
+++ zsh-cwd-history.zsh
@@ -37,15 +37,23 @@ ZSH_CWD_HISTORY_SHOW_CHPWD_MSG=false
 # Histfile Helper Functions                                          #
 #--------------------------------------------------------------------#
 
+# Universal (Linux/BSD) md5 utility
+_zsh_cwd_history_md5() {
+	which md5 > /dev/null 2>&1 && md5 -q || (md5sum | cut -c-32)
+}
+
 # Echo the name of the history file to use for current working dir
 # Uses the md5 hash of the absolute path
 _zsh_cwd_history_histfile_for_cwd() {
-	echo "$ZSH_CWD_HISTORY_DIR/.hist-$(echo "${PWD:A}" | md5 -q)"
+	echo "$ZSH_CWD_HISTORY_DIR/.hist-$(echo "${PWD:A}" | _zsh_cwd_history_md5)"
 }
 
 # Switch to a new history file, writing to the old one
 _zsh_cwd_history_switch_histfile() {
 	local new_histfile=$1
+
+	# Assert the directory exists
+	mkdir -p "$ZSH_CWD_HISTORY_DIR"
 
 	# Write old histfile
 	fc -P
